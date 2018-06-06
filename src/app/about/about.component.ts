81182cfdc54d0c30f07a0a5df4462e3d3474195e
kogerr <kogerr@hotmail.com>
2018-06-06 22:58:52 +0200

    more css on about page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -18,7 +18,17 @@ export class AboutComponent implements OnInit {
   constructor(private dataService: DataService, private route: ActivatedRoute, private titleService: Title) { }
 
   ngOnInit() {
-    [this.clients, this.arsPoetica1, this.arsPoetica2, this.awards, this.publications] = [[], '[]', '[]', [], []];
+    [this.clients, this.arsPoetica1, this.arsPoetica2, this.awards, this.publications] = [
+    ['Lorem Ipsum', 'Dolor Sit Amet', 'Consectetur Laboris Nisi', 'Adipiscing Elit', 'Exercitation Ullamco',
+      'Aaliquip Commodo', 'Consequat Duis', 'Occaecat Cupidatat', 'Non Proident', 'Sunt in Culpa', 'Qui Officia',
+      'Deserunt Mollit', 'Anim Estlaborum', 'Aaliquip Commodo', 'Lorem Ipsum', 'Dolor Sit Amet', 'Consectetur Laboris Nisi',
+      'Adipiscing Elit', 'Exercitation Ullamco'],
+      `I\'m a freelance graphic designer from Budapest, Hungary. I have an MA degree in Graphic Design
+      from the Hungarian University of Fine Arts. Mainly focus on branding, identities, logos and trademarks.
+      The goal of my work is to create simple, minimal and elegant creative solutions that are unique for the
+      client and represent the value of the firm.`, `My mentality about graphics is about making things easily
+      recognisable, simple, clean work suggesting continuity, eternity using traditional tools next to the
+      systematized appearance.`, [], []];
   }
 
 }
