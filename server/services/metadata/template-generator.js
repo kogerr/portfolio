a4bda6d50d5be7e11ebad7e5691567284a3ede97
kogerr <kogerr@hotmail.com>
2018-07-01 17:21:03 +0200

    changed template to template literal

--- server/services/metadata/template-generator.js
+++ server/services/metadata/template-generator.js
@@ -2,20 +2,12 @@ let template = require('./template.json');
 
 const workBaseURL = 'http://199.247.23.37/work/';
 const coverBaseURL = 'http://199.247.23.37/images/cover/';
-const titleplaceholder = 'titleplaceholder';
-const urlplaceholder = 'urlplaceholder';
-const imageplaceholder = /imageplaceholder/g;
-const descriptionplaceholder = 'descriptionplaceholder';
 
 exports.assembleResponse = function(post) {
     let title = post.title;
     let description = post.intro;
     let fullImageURL = coverBaseURL + post.cover;
     let fullTitleURL = workBaseURL + post.titleURL;
-    let response = template.text
-                    .replace(titleplaceholder, title)
-                    .replace(urlplaceholder, fullTitleURL)
-                    .replace(imageplaceholder, fullImageURL)
-                    .replace(descriptionplaceholder, description);
+    let response = eval(template.text);
     return response;
 };
