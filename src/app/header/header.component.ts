a37e3339a3c46d6ab7d77ad64dfb0f2b581f1a90
kogerr <kogerr@hotmail.com>
2018-10-06 18:03:38 +0200

    more CSS mainly about page

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -49,6 +49,7 @@ export class HeaderComponent {
 
   handleRouteChange(navigationStart: NavigationStart): any {
     window.scroll(0, 0);
+    this.mobileMenu = false;
     this.yellow = navigationStart.url.startsWith('/work/');
   }
 
