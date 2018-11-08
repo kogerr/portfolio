f28ea403821278789ab8dd77e32b453e4a4f915d
Gergely Kovacs <Gergely_Kovacs3@epam.com>
2018-11-08 15:56:10 +0100

    correct email decoding

--- server/services/email-parser.ts
+++ server/services/email-parser.ts
@@ -2,11 +2,11 @@ import { decodeQuotedPrintable, decodeLatin2} from './decoder';
 
 export let parseMail = (raw: string) => {
     let latin2 = raw.includes('8859-2');
-    let subject = findBetween(raw, '\\r\\nSubject', '\\r\\n');
-    let aroundHtmlContent = findBetween(raw, 'Content-Type: text/html;', '--\\r\\n');
+    let subject = findBetween(raw, '\r\nSubject', '\r\n');
+    let aroundHtmlContent = findBetween(raw, 'Content-Type: text/html;', '--\r\n');
     let htmlContent = findBetween(aroundHtmlContent, '</head>', '</html>');
     let aroundTextContent = findBetween(raw, 'Content-Type: text/plain;', 'Content-Type: text/html;');
-    let textContent = findBetween(aroundTextContent, 'quoted-printable', '\\r\\n--');
+    let textContent = findBetween(aroundTextContent, 'quoted-printable', '\r\n--');
 
     [subject, htmlContent, textContent] = [subject, htmlContent, textContent].map(text => {
         if (text) {
@@ -14,6 +14,13 @@ export let parseMail = (raw: string) => {
                 .replace(/\\r/g, '\r')
                 .replace(/\\n/g, '\n')
                 .replace(/\\"/g, '\"');
+
+            try {
+                decodedText = decodeURIComponent(escape(decodedText));
+            } catch (err) {
+                console.error(err);
+            }
+
             return latin2 ? decodeLatin2(decodedText) : decodedText;
         }
     });
